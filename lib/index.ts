f39fc54ef43a25d3f83a93eca7ffe90cf614d5fb
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-10-03 16:41:00 +0200

    Add tests
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,12 @@
 export function loadState<T>(app, key): T {
     const elem = <HTMLInputElement>document.querySelector(`#initial-state-${app}-${key}`)
     if (elem === null) {
-        const msg = `Could not find initial state ${key} of ${app}`
-        console.error(msg)
-        throw new Error(msg)
+        throw new Error(`Could not find initial state ${key} of ${app}`)
     }
 
     try {
         return JSON.parse(atob(elem.value))
     } catch (e) {
-        const msg = `Could not parse initial state ${key} of ${app}`
-        console.error(msg)
-        throw new Error(msg)
+       throw new Error(`Could not parse initial state ${key} of ${app}`)
     }
 }
