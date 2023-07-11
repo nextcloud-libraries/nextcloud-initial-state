7ce59f9ba0e39ed669625d5d0f6df4ff58ac2409
Ferdinand Thiessen <opensource@fthiessen.de>
2023-07-11 15:03:34 +0200

    chore: Lint Typescript files using ESLint and add CI workflow linting
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/index.ts
+++ lib/index.ts
@@ -5,17 +5,17 @@
  * @throws if the key can't be found
  */
 export function loadState<T>(app: string, key: string, fallback?: T): T {
-    const elem = <HTMLInputElement>document.querySelector(`#initial-state-${app}-${key}`)
-    if (elem === null) {
-        if (fallback !== undefined) {
-            return fallback
-        }
-        throw new Error(`Could not find initial state ${key} of ${app}`)
-    }
+	const elem = <HTMLInputElement>document.querySelector(`#initial-state-${app}-${key}`)
+	if (elem === null) {
+		if (fallback !== undefined) {
+			return fallback
+		}
+		throw new Error(`Could not find initial state ${key} of ${app}`)
+	}
 
-    try {
-        return JSON.parse(atob(elem.value))
-    } catch (e) {
-       throw new Error(`Could not parse initial state ${key} of ${app}`)
-    }
+	try {
+		return JSON.parse(atob(elem.value))
+	} catch (e) {
+		throw new Error(`Could not parse initial state ${key} of ${app}`)
+	}
 }
